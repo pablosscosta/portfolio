67f4aee8dbbcbc709fe6a735ca07612fc1f7bb88
Seu nome <dev.pablocosta@gmail.com>
2025-04-18 15:54:16 -0300

    Alterando visual da página dark e minimalista

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// script.js
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+  let currentSection = '';
+
+  // Identificando qual seção está visível
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  // Destacando o link ativo no menu
+  navLinks.forEach(link => {
+    link.classList.remove('text-blue-600', 'bg-blue-200'); // Remove destaque anterior
+    if (link.getAttribute('href').includes(currentSection)) {
+      link.classList.add('text-blue-600', 'bg-blue-200'); // Adiciona destaque à seção ativa
+    }
+  });
+});
